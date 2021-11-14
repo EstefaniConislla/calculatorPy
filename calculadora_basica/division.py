beb7c3f892fbffdb82dd3f162333be77f618f117
EstefaniConislla <estefaniyulianaca@gmail.com>
2021-11-14 18:13:24 -0500

    Its done

---
+++ calculadora_basica/division.py
@@ -0,0 +1,9 @@
+print("Operación dividir")
+# Declaro variable
+num1 = input("Ingresa un número: ")
+num2 = input("Ingresa otro número: ")
+
+res = float(num1) / float(num2)
+
+# Se imprime resultado
+print("La división es: ", res)
---
+++ calculadora_basica/multiplik.py
@@ -0,0 +1,9 @@
+print("Operación multiplicar")
+# Declaro variable
+num1 = input("Ingresa un número: ")
+num2 = input("Ingresa otro número: ")
+
+res = int(num1) * int(num2)
+
+# Se imprime resultado
+print("La multiplicación es: ", res)
