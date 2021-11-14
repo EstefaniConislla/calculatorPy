9e5006d82666c5ecb02249a4f352bb923bcdf1ff
EstefaniConislla <estefaniyulianaca@gmail.com>
2021-11-13 23:11:24 -0500

    operaciones basicas

---
+++ calculadora_basica/suma.py
@@ -0,0 +1,9 @@
+print("Operación suma")
+# Declaro variable
+num1 = input("Ingresa un número: ")
+num2 = input("Ingresa otro número: ")
+
+res = float(num1) + float(num2)
+
+# Se imprime resultado
+print("La suma es: ", res)
